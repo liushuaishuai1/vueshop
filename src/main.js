1c0634cffcc33ec030b6c1659aa9a9dcf02a935e
liushuaishuai1 <1349979336@qq.com>
2019-04-22 11:47:31 +0800

    登陆功能开发完毕

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import './assets/css/global.css'
-// Vue.config.productionTip = false
+// Vue.config.productionTip = falseMP
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+Vue.use(ElementUI)
+axios.defaults.baseURL='http://127.0.0.1:11333/api/private/v1/'
+Vue.prototype.$http=axios
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Home from '@/components/Home'
 
 Vue.use(Router) 
-
-export default new Router({
+const router =new Router({
   routes: [
-    {
-      path: '/Login',component:Login
-     
-    }
+    {path: '/Login',component:Login},
+    {path:'/home',component:Home}
   ]
 })
+router.beforeEach((to,from,next)=>{
+  var token=window.sessionStorage.getItem('token')
+  if(token ===null && to.path !=='/login'){
+     return next('/login')
+  }
+  next()
+})
+
+export default router
